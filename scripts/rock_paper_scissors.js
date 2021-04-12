43241ca2728246da288e1b2bfbecd24c18b5e234
Gzman <ganzmann@protonmail.com>
2021-04-12 13:14:52 +0200

    Reworked index.css and index.js

--- scripts/rock_paper_scissors.js
+++ scripts/rock_paper_scissors.js
@@ -1,60 +1,105 @@
 
-const ROCK = "rock";
-const PAPER = "paper";
-const SCISSORS = "scissors";
 
-const isValid = (input = "") => {
-    input = input.toLowerCase();
-    return input === ROCK || input === PAPER || input === SCISSORS;
-}
+const SELECTIONS = ["rock", "paper", "scissors"];
+const ROCK = SELECTIONS[0];
+const PAPER = SELECTIONS[1];
+const SCISSORS = SELECTIONS[2];
+const MAX_POINTS = 5;
 
-const userPlay = (e) => {
-    const btn = e.target;
-    const selection = btn.getAttribute("data-selection");
-    game(selection);
-}
+const playerPointsElement = document.querySelector(".player-points");
+const computerPointsElement = document.querySelector(".computer-points");
+const playerSelectionElement = document.querySelector(".player-selection");
+const computerSelectionElement = document.querySelector(".computer-selection");
+const outcomeElement = document.querySelector(".outcome");
 
-const computerPlay = () => {
-    const minOptions = 1;
-    const maxOptions = 3;
-    const selection = Math.floor(Math.random() * (maxOptions - minOptions + 1)) + minOptions;
-    switch (selection) {
-        case 1: return ROCK;
-        case 2: return PAPER;
-        case 3: return SCISSORS;
+function play(event) {
+    const playerSelection = event.target.getAttribute("data-selection");
+    const computerSelection = computerPlay();
+    displaySelections(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    if (isGameOver()) {
+        (playerPointsElement.textContent > computerPointsElement.textContent) ?
+            alert("Congratulations! You won the game") : alert("You lost the game");
+        resetGameState();
     }
 }
 
-const hasCanceled = (playerSelection) => playerSelection === null;
+function computerPlay() { return SELECTIONS[Math.floor(Math.random() * SELECTIONS.length)]; }
+
+function displaySelections(player, computer) {
+    playerSelectionElement.classList.remove(ROCK, PAPER, SCISSORS);
+    playerSelectionElement.classList.add(player);
+    playerSelectionElement.classList.add("selection-changed-animation");
+    computerSelectionElement.classList.remove(ROCK, PAPER, SCISSORS);
+    computerSelectionElement.classList.add(computer);
+    computerSelectionElement.classList.add("selection-changed-animation");
+}
+
+function playRound(playerSelection, computerSelection) {
+    if      (isDraw(playerSelection, computerSelection)) drawRound();
+    else if (hasWon(playerSelection, computerSelection)) playerWonRound();
+    else    computerWonRound();
+}
 
 const hasWon = (playerSelection, computerSelection) =>
-    (playerSelection == ROCK     && computerSelection == SCISSORS) ||
-    (playerSelection == PAPER    && computerSelection == ROCK) ||
-    (playerSelection == SCISSORS && computerSelection == PAPER);
+    (playerSelection === ROCK       && computerSelection === SCISSORS) ||
+    (playerSelection === PAPER      && computerSelection === ROCK) ||
+    (playerSelection === SCISSORS   && computerSelection === PAPER);
 
 const isDraw = (playerSelection, computerSelection) => playerSelection === computerSelection;
 
-const playRound = (playerSelection, computerSelection) => {
-    if (hasCanceled(playerSelection))                return null;
-    if (isDraw(playerSelection, computerSelection))  return `Draw! ${playerSelection} === ${computerSelection}`;
-    if (hasWon(playerSelection, computerSelection))  return `You won! ${playerSelection} beats ${computerSelection}`;
-    return `You lost! ${computerSelection} beats ${playerSelection}`;
+const drawRound = () => outcomeElement.textContent = "Draw";
+
+const playerWonRound = () => {
+    let points = parseInt(playerPointsElement.textContent);
+    playerPointsElement.textContent = ++points;
+    playerPointsElement.classList.add("player-points-animation");
+    outcomeElement.classList.add("player-points-animation");
+    outcomeElement.textContent = "You won";
 }
 
-const game = (userSelection) => {
-        const playerSelection = userSelection;
-        const computerSelection = computerPlay();
-        const outcome = playRound(playerSelection, computerSelection);
-        const resultList = document.querySelector(".result ul");
-        const result = document.createElement("li");
-        result.textContent = outcome;
-        resultList.append(result);
+const computerWonRound = () => {
+    let points = parseInt(computerPointsElement.textContent);
+    computerPointsElement.textContent = ++points;
+    computerPointsElement.classList.add("computer-points-animation");
+    outcomeElement.classList.add("computer-points-animation");
+    outcomeElement.textContent = "You lost";
 }
 
-const btns = document.querySelectorAll(".btn");
-btns.forEach((btn) => {
-    btn.addEventListener("click", userPlay);
-});
+function resetGameState() { window.location.reload(); }
 
+function isGameOver() {
+    const pPoints = parseInt(playerPointsElement.textContent);
+    const cPoints = parseInt(computerPointsElement.textContent);
+    return pPoints === MAX_POINTS || cPoints === MAX_POINTS;
+}
 
+function animationEnd(event) {
+    if (event.propertyName !== "transform") return;
+    const element = event.target;
+    if (element.matches(".player-selection") || element.matches(".computer-selection")) element.classList.remove("selection-changed-animation");
+    if (element.matches(".selection-button")) element.classList.remove("selection-button-animation");
+    if (element.matches(".outcome")) {
+        element.classList.remove("player-points-animation");
+        element.classList.remove("computer-points-animation");
+    }
+    if (element.matches(".player-points") || element.matches(".computer-points")) {
+        element.classList.remove("player-points-animation");
+        element.classList.remove("computer-points-animation");
+    }
+}
 
+function initGame() {
+    document.querySelectorAll(".selection-button").forEach((button) => {
+        button.addEventListener("click", play);
+        button.addEventListener("click", e => e.target.classList.add("selection-button-animation"));
+        button.addEventListener("transitionend", animationEnd);
+    });
+    playerPointsElement.addEventListener("transitionend", animationEnd);
+    playerSelectionElement.addEventListener("transitionend", animationEnd);
+    computerSelectionElement.addEventListener("transitionend", animationEnd);
+    computerPointsElement.addEventListener("transitionend", animationEnd);
+    outcomeElement.addEventListener("transitionend", animationEnd);
+}
+
+initGame();
